421ea3c76013b54c496c68d341158ef37ba30641
avkabanov <usrobotics1>
2019-12-09 17:30:00 +0300

    initial commit

---
+++ src/main/java/com/kabanov/messaging/Application.java
@@ -0,0 +1,11 @@
+package com.kabanov.messaging;
+
+/**
+ * @author Kabanov Alexey
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
